b5c70aa259bac156a626f5d30e985ebcb3200f1a
sanghyun0603 <110641935+sanghyun0603@users.noreply.github.com>
2022-11-16 01:21:18 +0900

    pjt

---
+++ back-end/community/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+#FloatField form말고 model에서 제어값설정?
+class MinMaxFloat(models.FloatField):
+    def __init__(self, min_value=None, max_value=None, *args, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        super(MinMaxFloat, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value' : self.max_value}
+        defaults.update(kwargs)
+        return super(MinMaxFloat, self).formfield(**defaults)
+
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    #search기능할예정 추후 수정 가능성 높음
+    movie_title = models.CharField(max_length=50)
+    review = models.TextField()
+    poster_path = models.CharField(max_length=200)
+    score = MinMaxFloat(min_value=0.0, max_value=5.0)
+
+class ReviewComment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+
+class Forum(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    #map
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ForumComment(models.Model):
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    
---
+++ back-end/movies/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+class Actor(models.Model):
+    name = models.CharField(max_length=50)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    release_date = models.DateField()
+    popularity = models.FloatField()
+    vote_count = models.IntegerField()
+    vote_average = models.FloatField()
+    overview = models.TextField()
+    poster_path = models.CharField(max_length=200)
+    youtube_key = models.CharField(max_length=100)
+    genres = models.ManyToManyField(Genre)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_movies")
+    actors = models.ManyToManyField(Actor)
